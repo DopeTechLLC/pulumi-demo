6002083e6161305c3a9012e8eea2a003607f75cc
Stefan Asseg <stefan@dopetech.com>
2025-06-21 20:00:17 +0000

    Commit + push all files

---
+++ index.ts
@@ -0,0 +1,59 @@
+import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
+import * as fs from "fs";
+import * as mime from "mime";
+
+const siteBucket = new aws.s3.Bucket(`pulumi-demo-${pulumi.getStack()}`, {
+    tags: {
+        env: "dev"
+    },
+    website: {
+        indexDocument: "index.html",
+    },
+});
+
+const siteBucketWebsiteConfig = new aws.s3.BucketWebsiteConfigurationV2("s3-website-bucket-config", {
+    bucket: siteBucket.id,
+    indexDocument: {
+        suffix: "index.html",
+    },
+});
+
+const publicAccessBlock = new aws.s3.BucketPublicAccessBlock("public-access-block", {
+    bucket: siteBucket.id,
+    blockPublicAcls: false,
+});
+
+const siteDir = "www"; // directory for content files
+
+// For each file in the directory, create an S3 object stored in `siteBucket`
+for (const item of fs.readdirSync(siteDir)) {
+    const filePath = require("path").join(siteDir, item);
+    const siteObject = new aws.s3.BucketObject(item, {
+        bucket: siteBucket,                               // reference the s3.Bucket object
+        source: new pulumi.asset.FileAsset(filePath),     // use FileAsset to point to a file
+        contentType: mime.getType(filePath) || undefined, // set the MIME type of the file
+    });
+}
+
+// Set the access policy for the bucket so all objects are readable
+const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
+    bucket: siteBucket.id, // refer to the bucket created earlier
+    policy: pulumi.jsonStringify({
+        Version: "2012-10-17",
+        Statement: [{
+            Effect: "Allow",
+            Principal: "*",
+            Action: [
+                "s3:GetObject",
+            ],
+            Resource: [
+                pulumi.interpolate `${siteBucket.arn}/*`,
+            ],
+        }],
+    }),
+}, { dependsOn: publicAccessBlock });
+
+// Stack exports
+export const bucketName = siteBucket.bucket;
+export const websiteUrl = siteBucketWebsiteConfig.websiteEndpoint;
